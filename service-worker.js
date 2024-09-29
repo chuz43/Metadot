c05c766f48b0c9a01ab23473b335ad058305ae6c
Jesus Amador <jesusac1992@gmail.com>
2024-09-29 00:58:28 -0600

    Update service-worker.js
    Adding Partial Updates

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,52 @@
-const CACHE_NAME = 'Metadot';
-const urlsToCache = [
-  '/Metadot/',
-  '/Metadot/index.html',
-  '/Metadot/manifest.json'
+// sw.js
+const CACHE_NAME = 'metadot-cache-v1';
+const RESOURCES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/Metadot/icon-192x192.png',
+  '/Metadot/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(RESOURCES_TO_CACHE))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          // Return cached version
+          return cachedResponse;
         }
-        return fetch(event.request);
+        return fetch(event.request).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
       })
   );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
